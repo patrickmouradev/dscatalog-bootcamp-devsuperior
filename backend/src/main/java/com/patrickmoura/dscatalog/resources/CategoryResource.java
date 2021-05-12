dae94034dc995e9c1723aa308381797463e9f908
Patrick Moura <patrickmoura@gmail.com>
2021-05-12 20:19:39 -0300

    H2 database

--- backend/src/main/java/com/patrickmoura/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/patrickmoura/dscatalog/resources/CategoryResource.java
@@ -3,6 +3,7 @@ package com.patrickmoura.dscatalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patrickmoura.dscatalog.entities.Category;
+import com.patrickmoura.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+	@Autowired
+	CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findall (){
 		
-		List<Category> list = new ArrayList<>();
-		
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		List<Category> list = service.findAll();
+
 		
 		return new ResponseEntity<>(list,HttpStatus.OK);
 		
