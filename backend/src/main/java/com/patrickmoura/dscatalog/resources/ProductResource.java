2ee0778fbb45679a5a23def5b3007972f83aa328
Patrick Moura <patrickmoura@gmail.com>
2021-05-24 16:15:40 -0300

    Refactor Pegeable

--- backend/src/main/java/com/patrickmoura/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/patrickmoura/dscatalog/resources/ProductResource.java
@@ -1,12 +1,10 @@
 package com.patrickmoura.dscatalog.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -31,14 +28,8 @@ public class ProductResource {
 	ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findall (
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			 ){
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
-		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<ProductDTO>> findall (Pageable pageable){
+		Page<ProductDTO> list = service.findAllPaged(pageable);
 		return new ResponseEntity<>(list,HttpStatus.OK);
 	}
 	
