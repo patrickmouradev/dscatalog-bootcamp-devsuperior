22ba6975b398646c9e289e9af152156e32e72d5f
Patrick Moura <patrickmoura@gmail.com>
2022-07-24 15:26:55 -0300

    NavBar Style

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
-import './assets/styles/custom.scss'
+import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <h1>Hello DSCatalog</h1>
+    </>
   );
 }
 
