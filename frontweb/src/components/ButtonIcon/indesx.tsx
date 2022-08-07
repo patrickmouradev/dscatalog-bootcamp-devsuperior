b361d12f890f093d738d9eafe143dd6614d48e80
Patrick Moura <patrickmoura@gmail.com>
2022-08-07 18:19:50 -0300

    Customização do Button Icon

--- frontweb/src/components/ButtonIcon/indesx.tsx
+++ frontweb/src/components/ButtonIcon/indesx.tsx
@@ -1,11 +1,22 @@
 import './styles.css'
-const ButtonIcon = () => {
-  return(
-      <button className={"btn btn-primary btn-icon"}>
-        <p>INICIE AGORA A SUA BUSCA</p>
+import {ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
 
-      </button>
-  )
+const ButtonIcon = () => {
+    return (
+
+        <div className={"btn-container"}>
+
+            <button className={"btn btn-primary"}>
+                <h6>INICIE AGORA A SUA BUSCA</h6>
+            </button>
+
+            <div className={"btn-icon-container"}>
+                <ArrowIcon/>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default ButtonIcon;
