f5a48db90f928833584dc13dc8362b600fc486c5
Patrick Moura <patrickmoura@gmail.com>
2022-12-01 21:11:48 -0300

    Rotas Auth

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Calatog from "./pages/Calatog";
 import Admin from "./pages/Admin";
 import ProductDetails from "./pages/ProductDetails";
+import Auth from "./pages/Admin/Auth/indesx";
 
 const Routes = () => {
     return (
@@ -19,13 +20,17 @@ const Routes = () => {
                 <Route path={"/products/:productId"}>
                     <ProductDetails/>
                 </Route>
-                <Redirect from={"/admin"} to={"/admin/products"} exact />
-                    <Route path={"/admin"}>
-                        <Admin/>
-                    </Route>
+                <Redirect from={"/admin/auth"} to={"/admin/auth/login"} exact/>
+                <Route path={"/admin/auth"}>
+                    <Auth/>
+                </Route>
+                <Redirect from={"/admin"} to={"/admin/products"} exact/>
+                <Route path={"/admin"}>
+                    <Admin/>
+                </Route>
             </Switch>
         </BrowserRouter>
-);
+    );
 }
 
 export default Routes;
